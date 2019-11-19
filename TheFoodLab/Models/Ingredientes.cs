13fdb5e492ff70f9dac113c75ebefff006cb5d86
MATIAS WAICMAN <44552861@comunidadort.edu.ar>
2019-11-19 12:09:40 -0300

    Cambios Mati Index

---
+++ TheFoodLab/Models/Ingredientes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheFoodLab.Models
+{
+    public class Ingredientes
+    {
+        private int _IdIngrediente;
+        private string _Nombre;
+
+        public int IdIngrediente { get => _IdIngrediente; set => _IdIngrediente = value; }
+        public string Nombre { get => _Nombre; set => _Nombre = value; }
+
+        public Ingredientes(int id, string nom)
+        {
+            IdIngrediente = id;
+            Nombre = nom;
+        }
+
+        public Ingredientes()
+        {
+
+        }
+    }
+}
