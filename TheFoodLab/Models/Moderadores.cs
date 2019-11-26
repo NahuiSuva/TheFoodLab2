6367e902707c8a01e30975a6bcf9f7e43291b942
GONZALO DAN SLUCKI <44593656@comunidadort.edu.ar>
2019-11-26 10:41:18 -0300

    Commit de Slucki

--- TheFoodLab/Models/Moderadores.cs
+++ TheFoodLab/Models/Moderadores.cs
@@ -8,9 +8,11 @@ namespace TheFoodLab.Models
 {
     public class Moderadores
     {
+
             private string _Username;
             private string _Password;
 
+            public int IdModeradores { get; set; }
             [Required(ErrorMessage = "Ingresá un usuario valido!!!")]
             public string Username { get => _Username; set => _Username = value; }
             [Required(ErrorMessage = "Ingresá una contraseña valida!!!")]
